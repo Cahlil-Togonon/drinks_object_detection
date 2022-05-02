6e95a70d92e0963be56b56085703af8445fd17bf
Cahlil-Togonon <100193550+Cahlil-Togonon@users.noreply.github.com>
2022-05-02 19:20:31 +0800

    changed link to latest model

--- gdrive_downloader.py
+++ gdrive_downloader.py
@@ -4,12 +4,12 @@ import shutil
 
 def download_model(model_path):
     if not os.path.exists(model_path):
-        url = 'https://drive.google.com/uc?id=1-JLadMe-gu89P808wb87w2dONlwmYV8g'
+        url = 'https://drive.google.com/uc?id=1hJCwL0hy-USoPuUd1JMv1IYpuQs1nifT'    # fasterrcnn_model_drinks_Epoch9.pt
         gdown.download(url, model_path, quiet=False)
 
 def download_dataset(data_path):
     if not os.path.exists(data_path):
-        url = 'https://drive.google.com/uc?id=1AdMbVK110IKLG7wJKhga2N2fitV1bVPA'
+        url = 'https://drive.google.com/uc?id=1AdMbVK110IKLG7wJKhga2N2fitV1bVPA'    # drinks.tar.gz
         gdown.download(url, quiet=False)
         print("Unpacking Drinks Dataset...")
         shutil.unpack_archive('drinks.tar.gz')
