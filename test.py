9a3ab5c2fb5a644e117ae8b1a773e6bf8776caf1
Cahlil-Togonon <100193550+Cahlil-Togonon@users.noreply.github.com>
2022-05-02 16:08:25 +0800

    separated dataloader, incorporates helper tools

--- test.py
+++ test.py
@@ -1,8 +1,4 @@
-import os
-import json
 import torch
-from PIL import Image
-
 from torch.utils.data import DataLoader
 from torchvision import models
 from torchvision import transforms as T
@@ -10,75 +6,13 @@ from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
 import utils
 from engine import evaluate
-
-import gdown
-
-class DrinksDataset(torch.utils.data.Dataset):
-    def __init__(self, root, annotations_json, Transforms=None):
-        self.root = root
-        self.Transforms = Transforms
-
-        json_file = open(annotations_json)
-        data_json = json.load(json_file)
-        json_file.close()
-
-        self.imgs = []
-        self.targets = []
-        idx = 0
-        for keys in data_json["_via_img_metadata"]:
-            data = data_json["_via_img_metadata"][keys]
-            img_name = data['filename']
-            img_path = os.path.join(self.root, img_name)
-            img = Image.open(img_path).convert("RGB")
-
-            num_objs = len(data['regions'])
-            if num_objs == 0: continue
-            boxes = []
-            labels = []
-            area = []
-            for region in data['regions']:
-                box = region['shape_attributes']
-                xmin = box['x']
-                ymin = box['y']
-                xmax = box['x'] + box['width']
-                ymax = box['y'] + box['height']
-                area.append(box['width'] * box['height'])
-                boxes.append([xmin, ymin, xmax, ymax])
-                labels.append(int(region['region_attributes']['name']))
-
-            boxes = torch.as_tensor(boxes, dtype=torch.float32)
-            labels = torch.as_tensor(labels, dtype=torch.int64)
-            image_id = torch.tensor([idx])
-            area = torch.as_tensor(area)
-            iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
-            
-            target = {}
-            target["boxes"] = boxes
-            target["labels"] = labels
-            target["image_id"] = image_id
-            target["area"] = area
-            target["iscrowd"] = iscrowd
-
-            self.imgs.append(img)
-            self.targets.append(target)
-            idx += 1
-
-    def __getitem__(self, idx):
-        img = self.imgs[idx]
-        target = self.targets[idx]
-
-        if self.Transforms is not None:
-            img = self.Transforms(img)
-            #target = self.Transforms(target)
-
-        return img, target
-
-    def __len__(self):
-        return len(self.imgs)
+from dataloader import DrinksDataset
+from google_downloader import download_dataset, download_model
 
 if __name__ == '__main__':
-    data_path = "../drinks"
-    test_annotations_json = "../drinks/labels_test.json"
+    data_path = "drinks"
+    download_dataset(data_path)
+    test_annotations_json = "drinks/labels_test.json"
     test_dataset = DrinksDataset(data_path, test_annotations_json, T.ToTensor())
 
     test_dataloader = DataLoader(test_dataset,
@@ -98,10 +32,8 @@ if __name__ == '__main__':
     params = [p for p in model.parameters() if p.requires_grad]
     optimizer = torch.optim.SGD(params, lr=0.005,momentum=0.9, weight_decay=0.0005)
     
-    model_path = 'fasterrcnn_model_drinks_Epoch7.pt'        # edit epoch as needed
-    if not os.path.exists(model_path):
-        url = 'https://drive.google.com/u/1/uc?id=1-JLadMe-gu89P808wb87w2dONlwmYV8g'
-        gdown.download(url, model_path, quiet=False)
+    model_path = 'fasterrcnn_model_drinks_Epoch7.pt'      # edit epoch as needed
+    download_model(model_path)
 
     checkpoint = torch.load(model_path)
     model.load_state_dict(checkpoint['model_state_dict'])
--- train.py
+++ train.py
@@ -1,8 +1,4 @@
-import os
-import json
 import torch
-from PIL import Image
-
 from torch.utils.data import DataLoader
 from torchvision import models
 from torchvision import transforms as T
@@ -10,73 +6,13 @@ from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
 import utils
 from engine import train_one_epoch
-
-class DrinksDataset(torch.utils.data.Dataset):
-    def __init__(self, root, annotations_json, Transforms=None):
-        self.root = root
-        self.Transforms = Transforms
-
-        json_file = open(annotations_json)
-        data_json = json.load(json_file)
-        json_file.close()
-
-        self.imgs = []
-        self.targets = []
-        idx = 0
-        for keys in data_json["_via_img_metadata"]:
-            data = data_json["_via_img_metadata"][keys]
-            img_name = data['filename']
-            img_path = os.path.join(self.root, img_name)
-            img = Image.open(img_path).convert("RGB")
-
-            num_objs = len(data['regions'])
-            if num_objs == 0: continue
-            boxes = []
-            labels = []
-            area = []
-            for region in data['regions']:
-                box = region['shape_attributes']
-                xmin = box['x']
-                ymin = box['y']
-                xmax = box['x'] + box['width']
-                ymax = box['y'] + box['height']
-                area.append(box['width'] * box['height'])
-                boxes.append([xmin, ymin, xmax, ymax])
-                labels.append(int(region['region_attributes']['name']))
-
-            boxes = torch.as_tensor(boxes, dtype=torch.float32)
-            labels = torch.as_tensor(labels, dtype=torch.int64)
-            image_id = torch.tensor([idx])
-            area = torch.as_tensor(area)
-            iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
-            
-            target = {}
-            target["boxes"] = boxes
-            target["labels"] = labels
-            target["image_id"] = image_id
-            target["area"] = area
-            target["iscrowd"] = iscrowd
-
-            self.imgs.append(img)
-            self.targets.append(target)
-            idx += 1
-
-    def __getitem__(self, idx):
-        img = self.imgs[idx]
-        target = self.targets[idx]
-
-        if self.Transforms is not None:
-            img = self.Transforms(img)
-            #target = self.Transforms(target)
-
-        return img, target
-
-    def __len__(self):
-        return len(self.imgs)
+from dataloader import DrinksDataset
+from google_downloader import download_dataset
 
 if __name__ == '__main__':
-    data_path = "../drinks"
-    train_annotations_json = "../drinks/labels_train.json"
+    data_path = "drinks"
+    download_dataset(data_path)
+    train_annotations_json = "drinks/labels_train.json"
     train_dataset = DrinksDataset(data_path, train_annotations_json, T.ToTensor())
 
     train_dataloader = DataLoader(train_dataset,
@@ -97,10 +33,14 @@ if __name__ == '__main__':
     optimizer = torch.optim.SGD(params, lr=0.005,momentum=0.9, weight_decay=0.0005)
     lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
 
-    # checkpoint = torch.load("fasterrcnn_model_drinks_Epoch7.pt")      # edit epoch as needed
+    # model_path = 'fasterrcnn_model_drinks_Epoch7.pt'      # edit epoch as needed
+    # download_model(model_path)
+
+    # checkpoint = torch.load(model_path)
     # model.load_state_dict(checkpoint['model_state_dict'])
     # optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
     # epoch = checkpoint['epoch']
+    
     epoch = 0
     num_epochs = 10
     model.to(device)
@@ -108,7 +48,6 @@ if __name__ == '__main__':
     while epoch < num_epochs:
         train_one_epoch(model, optimizer, train_dataloader, device, epoch, print_freq=100)
         lr_scheduler.step()
-        #evaluate(model, data_loader_test, device=device)
 
         PATH = f"fasterrcnn_model_drinks_Epoch{epoch}.pt"
         torch.save({
